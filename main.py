5ed697a1694e9937e3f090873b84b0f514e09ed0
vladislav <vl_evlashin@mail.ru>
2023-01-22 22:20:43 +0300

    Add task exam

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # Is Number even?
 
-number = int(input('Input number: '))
+array = [3,6,10,15,18,24,29,35]
+correct = []
+max = 0
+def is_even(nums):
+    global max
+    for i in nums:
+        if i % 6 == 0:
+            correct.append(i)
+            if i > max:
+                max = i
+    return max
 
-def is_even(num):
-    if num % 2 == 0:
-        print('Number is even!')
-    else:
-        print('Number is not even!')
-
-is_even(number)
+is_even(array)
+print(correct)
+print(max)
